64728fd6b0ec6699066a27ede57630aa81f8caf4
Mani <rkmanikarthick@gmail.com>
2017-07-04 11:15:50 +0530

    Initial commit

---
+++ src/main/java/com/example/sunshine/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sunshine.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
